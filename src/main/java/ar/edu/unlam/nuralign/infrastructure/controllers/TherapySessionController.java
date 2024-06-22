d19647925bc234535777ba28fb311aa6fa09881b
CrisGadea <cristianhernangadea@gmail.com>
2024-06-22 04:28:52 -0300

    HOTFIX V3.4.1: Fix TherapySession Bug

--- src/main/java/ar/edu/unlam/nuralign/infrastructure/controllers/TherapySessionController.java
+++ src/main/java/ar/edu/unlam/nuralign/infrastructure/controllers/TherapySessionController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/TherapySession")
+@RequestMapping("/api/therapySession")
 @CrossOrigin (origins = "*")
 public class TherapySessionController {
 
